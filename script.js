998c5d9dd92afb711c0280cc6f32ce4e29c51638
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-04-02 15:30:38 +0330

    update

--- script.js
+++ script.js
@@ -3,29 +3,50 @@ const wrapperElem = $.querySelector('.wrapper')
 const toastElem = $.querySelector('.toast')
 const titleElem = $.querySelector('span')
 const subTitleElem = $.querySelector('p')
-const icon = $.querySelector('icon')
+const icon = $.querySelector('.icon')
 const closeIcon = $.querySelector('.close-icon')
+const durationBarElem = $.querySelector('.duration-bar')
 
 const ajaxReq = () => {
     const url = "https://jsonplaceholder.typicode.com/posts"
     fetch(url).then(res => {
+        // console.log(res.status);
         if (res.status === 200 || res.status < 300) {
             titleElem.textContent = 'You\'r online now'
-            subTitleElem.textContent = 'Internet is connected.'
+            durationBarElem.style.backgroundColor = "#2ecc71"
+            subTitleElem.textContent = 'Internet is connected :)'
             toastElem.classList.remove('offline')
             setInterval(() => {
                 closeWrapper()
             }, 5000);
+            icon.innerHTML = '<i class="uil uil-wifi"></i>'
+        } else {
+            offline()
         }
     })
     .catch(err => {
-        console.error('=>' + err);
+        console.log(err);
+        offline()
     })
 }
 
+const offline = () => {
+    wrapperElem.classList.remove('hide')
+    durationBarElem.style.backgroundColor = "#ccc"
+    titleElem.textContent = 'You\'r offline now'
+    subTitleElem.textContent = 'Internet is not connected :('
+    toastElem.classList.add('offline')
+    setInterval(() => {
+        closeWrapper()
+    }, 5000);
+    icon.innerHTML = '<i class="uil uil-slash"></i>'
+}
 const closeWrapper = () => {
     wrapperElem.classList.add('hide')
 }
 
 closeIcon.addEventListener('click', closeWrapper)
-window.addEventListener('load', ajaxReq)
+window.addEventListener('load', () => {
+    ajaxReq()
+
+})
