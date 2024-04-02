8e0d701140f4418ab2a5716989e628dd3a24f6c4
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-04-02 14:06:06 +0330

    update script api fetch

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-fetch(window).then((response)=>{
-    console.log(response.status); // Will show you the status
-    if (!response.ok) {
-        throw new Error("HTTP status " + response.status);
+const $ = document
+const wrapperElem = $.querySelector('.wrapper')
+const toastElem = $.querySelector('.toast')
+const titleElem = $.querySelector('span')
+const subTitleElem = $.querySelector('p')
+const icon = $.querySelector('icon')
+const closeIcon = $.querySelector('.close-icon')
+
+window.addEventListener('load', () => {
+    const url = "https://jsonplaceholder.typicode.com/posts"
+    const ajaxReq = () => {
+        fetch(url).then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.error('=>' + err);
+        })
     }
-    return response.json();
-});
+
+    ajaxReq()
+})
