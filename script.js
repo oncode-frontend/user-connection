758474b25176f7034aa936e1e4703fe82a38232b
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-04-02 14:18:18 +0330

    update script

--- script.js
+++ script.js
@@ -6,16 +6,26 @@ const subTitleElem = $.querySelector('p')
 const icon = $.querySelector('icon')
 const closeIcon = $.querySelector('.close-icon')
 
-window.addEventListener('load', () => {
+const ajaxReq = () => {
     const url = "https://jsonplaceholder.typicode.com/posts"
-    const ajaxReq = () => {
-        fetch(url).then(res => {
-            console.log(res);
-        })
-        .catch(err => {
-            console.error('=>' + err);
-        })
-    }
+    fetch(url).then(res => {
+        if (res.status === 200 || res.status < 300) {
+            titleElem.textContent = 'You\'r online now'
+            subTitleElem.textContent = 'Internet is connected.'
+            toastElem.classList.remove('offline')
+            setInterval(() => {
+                closeWrapper()
+            }, 5000);
+        }
+    })
+    .catch(err => {
+        console.error('=>' + err);
+    })
+}
 
-    ajaxReq()
-})
+const closeWrapper = () => {
+    wrapperElem.classList.add('hide')
+}
+
+closeIcon.addEventListener('click', closeWrapper)
+window.addEventListener('load', ajaxReq)
